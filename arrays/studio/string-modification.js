8cbdc9a62f031bf668c437a6a1aa6748495b98dc
Your Name <you@example.com>
2024-05-28 21:48:26 -0500

    strings and arrays stud

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,22 +1,37 @@
 const input = require('readline-sync');
-let str = "LaunchCode";
+// let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-let newString = str.slice(3).concat(str.slice(0,3));
-console.log(newString);
+// const first3Letters = str.slice(0, 3);
+// const removedLetters = str.slice(3)
+// const newString = removedLetters + first3Letters;
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
-console.log(`The original string is ${str} and modified string is ${newString}`);
+
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-let query = input.question("Enter the number of letters?");
+
+// const str = input.question('What do you want to translate?');
+// const letterstoRelocate = input.question('How many letters to move?');
+// const first3Letters = str.slice(0, letterstoRelocate);
+// const removedLetters = str.slice(letterstoRelocate);
+// const newString = removedLetters + first3Letters;
+// console.log(`${newString} translates to ${str} in pseudo-pig latin.`);
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
-if(str.length > query){
-    console.log(`The ${query} letters are longer than the ${str}`);
-}else{
-    let newString = str.slice(3).concat(str.slice(0,3));
-console.log(newString);
-}else{
-    let newString = str.slice(query).concat(str.slice(0,query));
-console.log(newString);
+
+const str = input.question('What do you want to translate?');
+const letterstoRelocate = input.question('How many letters to move?');
+
+if (letterstoRelocate > str.length) {
+    const first3Letters = str.slice(0, letterstoRelocate);
+    const removedLetters = str.slice(letterstoRelocate);
+    const newString = removedLetters + first3Letters;
+    console.log(`Can't do that many letters! ${newString} translates to ${str} in pseudo-pig latin.`);
+
+} else { 
+
+const first3Letters = str.slice(0, letterstoRelocate);
+const removedLetters = str.slice(letterstoRelocate);
+const newString = removedLetters + first3Letters;
+console.log(`${newString} translates to ${str} in pseudo-pig latin.`);
 }
