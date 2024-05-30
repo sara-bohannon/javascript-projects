0eef525b8c2688d157e1a46a0f78388cc8ea6fd0
Your Name <you@example.com>
2024-05-30 12:28:06 -0500

    overdue

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -6,12 +6,12 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
 //2) 
-function reverseCommas(commaString) {
+function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	;
-	let check = commaString;
-	let output;
+	
+	let check = strings[0];
 	if (check.includes(",")) {
+		let output;
 		output = check.split(",").reverse().join(",");
 	}
 
@@ -22,7 +22,7 @@ function reverseCommas(commaString) {
 	//TODO: 2. write the code required for this step
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
-	return output;
+	return check;
 }
 
 //3)
